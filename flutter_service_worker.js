6276cdb3a795f3cdcf149670eafc0ecaa0b43196
dr-gwen <dr-gwen@users.noreply.github.com>
2021-03-16 16:37:37 +0000

    deploy: 27f9d6e2ce05ec069aef96d597e238949c1df333

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "manifest.json": "0d2ef5bf43d0a5d981e8400f48458541",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"feed.xml": "a267df05e69d2c3bba0d351069b82fe1",
+"feed.xml": "e4eda356bd0f0f93a0cb2df9bdb65793",
 "assets/NOTICES": "34ea86baf0d89f88498f7675e6a27674",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "0d266ffbe90dae02458487c9d33b7373",
