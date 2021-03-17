7ee4987d4a5bce612f1bba570403480a21359346
dr-gwen <dr-gwen@users.noreply.github.com>
2021-03-17 02:17:52 +0000

    deploy: 31cad64e26a28fadd2987eff97a2cf6f0b8966cf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "0d2ef5bf43d0a5d981e8400f48458541",
-"feed.xml": "bf4ad72017eac7e663fea1f5064582d2",
+"feed.xml": "4b68f578f83f1a574cf89e7c5647c9cb",
 "assets/NOTICES": "34ea86baf0d89f88498f7675e6a27674",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "0d266ffbe90dae02458487c9d33b7373",
 "assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"audio/01%20-%20Manuscripted%20-%20When%20scenes%20speak%20louder%20than%20words.mp3": "a7339081a7c0c6dc76880793be37804e",
-"audio/00%20-%20Manuscripted%20-%20Intro.mp3": "a0dc0f442b257fd1607a5ac59514ac2d",
+"audio/01-Manuscripted-WhenScenesSpeakLouderThanWords.mp3": "a7339081a7c0c6dc76880793be37804e",
+"audio/00-Manuscripted-Intro.mp3": "a0dc0f442b257fd1607a5ac59514ac2d",
 "index.html": "2dfdade837632d9630a6ca0d4a420179",
 "/": "2dfdade837632d9630a6ca0d4a420179",
 "main.dart.js": "83943554013effa7d8897a948d6994a2",
