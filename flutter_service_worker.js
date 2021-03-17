888e9c2ae9e778467ea7b655a7d597ee8867152f
dr-gwen <dr-gwen@users.noreply.github.com>
2021-03-17 15:43:16 +0000

    deploy: e1bc022816272785ff9da9a9b1488b70e96485a2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "0d2ef5bf43d0a5d981e8400f48458541",
-"feed.xml": "2492aa6122c7410689c82de1cf3b38ec",
+"feed.xml": "bcbb603c585bcb8687f95d9f870de4ed",
 "assets/NOTICES": "34ea86baf0d89f88498f7675e6a27674",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "0d266ffbe90dae02458487c9d33b7373",
