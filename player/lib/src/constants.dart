c74cb4905bb9baddd8977de1a4edd25b62180a93
Gwen Rehrig <10004248+dr-gwen@users.noreply.github.com>
2022-02-02 13:31:12 -0800

    fixing stuff (hopefully)

--- player/lib/src/constants.dart
+++ player/lib/src/constants.dart
@@ -1,3 +1,3 @@
 const kPodcastFeed =
     'https://dr-gwen.github.io/podcast/feed.xml';
-const kSpotifyLink = 'https://open.spotify.com/show/3UTiK34aDOOSHFpGQ0RglN';
+const kSpotifyLink = 'https://open.spotify.com/show/2Jvk6jz1VqD8OBxIDKOTYV';
